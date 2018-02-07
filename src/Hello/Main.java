686c08b04d344f1337550b059a820506da86b629
emmanuel <emmanuel@localhost.localdomain>
2018-02-07 13:31:24 -0600

    test2

--- src/Hello/Main.java
+++ src/Hello/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	private static Logger msg = Logger.getLogger("InfoLoggin");
 	
 	public static void main(String[] args) {
-		msg.info("Holla Mundo New1");
+		msg.info("Holla Mundo New2");
 	}
 	
 }
