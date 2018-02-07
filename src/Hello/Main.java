a11bd8215a681c93e5fafa76b59ff5098ba74863
emmanuel <emmanuel@localhost.localdomain>
2018-02-07 13:00:36 -0600

    first

---
+++ src/Hello/Main.java
@@ -0,0 +1,12 @@
+package Hello;
+import java.util.logging.Logger;
+
+public class Main {
+
+	private static Logger msg = Logger.getLogger("InfoLoggin");
+	
+	public static void main(String[] args) {
+		msg.info("Holla Mundo");
+	}
+	
+}
