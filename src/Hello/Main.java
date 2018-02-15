a2ba39418dc2babb23bbb715cdc2a6a7635ea531
emmanuel <emmanuel@localhost.localdomain>
2018-02-15 15:13:14 -0600

    Testing hook1

--- src/Hello/Main.java
+++ src/Hello/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	private static Logger msg = Logger.getLogger("InfoLoggin");
 	
 	public static void main(String[] args) {
-		msg.info("Holla Mundo New3");
+		msg.info("Holla Mundo New4");
 	}
 	
 }
